390d7b3142b3adae81736b3a903a7dd39d8f6e48
Bogdan Bujdea <bogdan@thewindev.net>
2021-02-26 01:48:52 +0200

    Changed urls

--- src/SmartHomeIntegrations.Server/Program.cs
+++ src/SmartHomeIntegrations.Server/Program.cs
@@ -15,6 +15,7 @@ namespace SmartHomeIntegrations.Server
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+
                 });
     }
 }
