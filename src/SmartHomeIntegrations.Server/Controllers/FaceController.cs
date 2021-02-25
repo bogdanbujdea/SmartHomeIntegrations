470aae375c1bd53c4d0a8d20b47cb3910d636ec8
Bogdan Bujdea <bogdan@thewindev.net>
2021-02-26 00:15:05 +0200

    Added server project

---
+++ src/SmartHomeIntegrations.Server/Controllers/FaceController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartHomeIntegrations.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FaceController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetOfficePersons()
+        {
+            return Ok(new
+            {
+                message = "Hello world"
+            });
+        }
+    }
+}
